7ee0863a538e54640bab1691126cbe2d9d2bc2f6
joaoDossena <joaopdossena@gmail.com>
2020-11-02 18:34:16 +0000

    Erasing comments from main

--- 1º Trabalho/src/main.c
+++ 1º Trabalho/src/main.c
@@ -31,31 +31,6 @@ int main(int argc, char *argv[])
     if (application.status == TRANSMITTER) strcpy(application.port, SERIAL_PORT_1);
     else if (application.status == RECEIVER) strcpy(application.port, SERIAL_PORT_2);
 
-    
-    // // TEST LLOPEN AND LLCLOSE
-    // int llopenReturn = llopen(port, application.status);
-    // printf("LLOPEN RETURN: %d\n", llopenReturn);
-    // if (llopenReturn < 0) 
-    //     exit(1);
-    // int llcloseReturn = llclose(application.fd);
-    // printf("LLCLOSE RETURN: %d\n", llcloseReturn);
-    
-
-    // // TEST PREPAREI
-    // frame_t **iFrames;
-    // char data[10];
-    // data[0] = 0x11;
-    // data[1] = 0x10;
-    // data[2] = 0x10;
-    // data[3] = 0x10;
-    // data[4] = 0x10;
-    // data[5] = 0x10;
-    // data[6] = 0x10;
-    // data[7] = 0x10;
-    // data[8] = 0x10;
-    // data[9] = 0x10;
-    // int fNeeded = prepareI(data, 10, &iFrames);
-    // for (int i = 0; i < fNeeded; i++) printFrame(iFrames[i]);
 
 
     // TEST DATA LAYER
@@ -83,32 +58,6 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-
-    // // TEST BYTE STUFFING
-    // frame_t frame;
-    // frame.bytes[0] = FLAG;
-    // frame.bytes[1] = TRANSMITTER_TO_RECEIVER;
-    // frame.bytes[2] = I;
-    // frame.bytes[3] = bccCalculator(frame.bytes, 1, 2);
-    // frame.bytes[4] = 5;
-    // frame.bytes[5] = 'o';
-    // frame.bytes[6] = 'l';
-    // frame.bytes[7] = 'a';
-    // frame.bytes[8] = '}';
-    // frame.bytes[9] = 'o';
-    // frame.bytes[10] = FLAG_LAST_FRAME;
-    // frame.bytes[11] = bccCalculator(frame.bytes, 4, frame.bytes[4] + 2);
-    // frame.bytes[12] = FLAG;
-    // frame.size = 13;
-    // printFrame(&frame);
-    // stuffFrame(&frame);
-    // printFrame(&frame);
-    // destuffFrame(&frame);
-    // printFrame(&frame);
-
-
-
     return 0;
-
 }
 
