c993f6c2bacad4c6c3c8d3435a08bb1d2f4620e3
joaoDossena <joaopdossena@gmail.com>
2020-12-04 17:28:23 +0000

    Validating number of arguments

--- proj2/src/main.c
+++ proj2/src/main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
-int main(int argc, char* argv[]){
-	
+int main(int argc, char *argv[]){
+	if (argc != 2){
+		perror("Wrong number of arguments!\nUsage: ./download <FTP url>\n");
+		return -1;
+	}
 
 	return 0;
 }
